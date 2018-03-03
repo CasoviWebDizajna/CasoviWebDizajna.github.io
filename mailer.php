a9a82cd32f1b266cd20c3f2849810237524827ef
Marina Bubnic <bubnic.marina@gmail.com>
2018-03-03 12:37:56 +0100

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -1,16 +1,29 @@
 <?php
-if($_POST) {
-  $to = "miricmilica35@gmail.com"; // your mail here
+                         
+if(filter_has_var(INPUT_POST, 'submit')) {
+
   $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
   $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
   $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
-  $body = "Message: $message\nE-mail: $email";
-  
-  if(@mail($to, $subject, $body)) {
-    $output = json_encode(array('success' => true));
-    die($output);
+
+  if(!empty($email) && !empty($subject) && !empty($message)) {
+    $to = "miricmilica35@gmail.com"; // your mail here
+    $body = "Message: $message\nE-mail: $email";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-Type:text/html;charset=UTF-8" . "\r\n";
+
+      if(mail($to, $subject, $headers, $message, $body)) {
+        $output = json_encode(array('success' => true));
+        die($output);
+      } else {
+        $output = json_encode(array('success' => false));
+        die($output);
+      }
+    
   } else {
-    $output = json_encode(array('success' => false));
-    die($output);
+    
+    echo "You must fill in all the required fields!";
+    
   }
-}?>
+};
+?>
